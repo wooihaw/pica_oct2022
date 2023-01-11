778b846f0810860fc6761952357b44f77f8b19e1
wooihaw <wooihaw.tan@gmail.com>
2023-01-11 17:19:19 +0800

    Added long processes

--- basic_gui.py
+++ basic_gui.py
@@ -1,4 +1,9 @@
 import PySimpleGUI as sg
+from time import sleep
+
+
+def long_process():
+    sleep(5)
 
 channels = tuple(range(1, 5))
 vscales = ('100mV', '200mV', '500mV', '1V', '2V', '5V')
@@ -12,7 +17,7 @@ layout = [  [sg.Text('Channel', size=text_size), sg.Combo(channels, default_valu
             [sg.Text('Vertical Offset', size=text_size), sg.Slider(range=(-5.0, 5.0), default_value=0.0, resolution=0.1, orientation='h', key='-voffset-', expand_x=True)],
             [sg.Text('Timebase scale', size=text_size), sg.Combo(tscales, key='-tscale-', default_value=tscales[3], expand_x=True)],
             [sg.Text('Timebase position', size=text_size), sg.Slider(range=(-5.0, 5.0), default_value=0.0, resolution=0.1, orientation='h', key='-tpos-', expand_x=True)],
-            [sg.Button('Connect', size=button_size), sg.Button('Send', size=button_size), sg.Button('Close', size=button_size)]
+            [sg.Button('Connect', size=button_size), sg.Button('Send', size=button_size, disabled=True), sg.Button('Close', size=button_size)]
 ]
 
 window = sg.Window('Oscilloscope', layout, element_justification='center')
@@ -21,5 +26,11 @@ while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Close'):
         break
+    elif event == 'Connect':
+        window['Send'].update(disabled=False)
+    elif event == 'Send':
+        window.perform_long_operation(lambda: long_process(), '-completed_long_process-')
+    elif event == '-completed_long_process-':
+        sg.Popup('Long process completed!')
 
 window.close()
